f5a259e03a25f93c801efabc9ddb9c293ec922c5
wulinlin <18574945291@163.com>
2023-11-28 14:37:39 +0800

    基本搞定

--- models/convert.go
+++ models/convert.go
@@ -39,3 +39,146 @@ func CoinTaskToObject(data *pb.TbCoinTask) *TbCoinTask {
 	d.SysUpdated = common.TimeParse(data.Start)
 	return d
 }
+
+func CoinUserToMessage(data *TbCoinUser) *pb.TbCoinUser {
+	d := &pb.TbCoinUser{
+		Id:         int32(data.Id),
+		Uid:        int32(data.Uid),
+		Coins:      int32(data.Coins),
+		SysCreated: "",
+		SysUpdated: "",
+	}
+	d.SysCreated = common.TimeFormat(data.SysCreated)
+	d.SysUpdated = common.TimeFormat(data.SysUpdated)
+	return d
+}
+func CoinDetailToMessage(data *TbCoinDetail) *pb.TbCoinDetail {
+	d := &pb.TbCoinDetail{
+		Id:         int32(data.Id),
+		Uid:        int32(data.Uid),
+		TaskId:     int32(data.TaskId),
+		Coin:       int32(data.Coin),
+		SysCreated: "",
+		SysUpdated: "",
+	}
+	d.SysCreated = common.TimeFormat(data.SysCreated)
+	d.SysUpdated = common.TimeFormat(data.SysUpdated)
+	return d
+}
+
+// CoinDetailToObject message转model
+func CoinDetailToObject(data *pb.TbCoinDetail) *TbCoinDetail {
+	d := &TbCoinDetail{
+		Id:         uint(data.Id),
+		Uid:        int(data.Uid),
+		TaskId:     int(data.TaskId),
+		Coin:       int(data.Coin),
+		SysCreated: nil,
+		SysUpdated: nil,
+	}
+	d.SysCreated = common.TimeParse(data.SysCreated)
+	d.SysUpdated = common.TimeParse(data.SysUpdated)
+	return d
+}
+
+// GradeInfoToMessage model转message
+func GradeInfoToMessage(data *TbGradeInfo) *pb.TbGradeInfo {
+	d := &pb.TbGradeInfo{
+		Id:          int32(data.Id),
+		Title:       data.Title,
+		Description: data.Description,
+		Score:       int32(data.Score),
+		Expired:     int32(data.Expired),
+		SysCreated:  "",
+		SysUpdated:  "",
+	}
+	d.SysCreated = common.TimeFormat(data.SysCreated)
+	d.SysUpdated = common.TimeFormat(data.SysUpdated)
+	return d
+}
+
+// GradeInfoToObject message转model
+func GradeInfoToObject(data *pb.TbGradeInfo) *TbGradeInfo {
+	d := &TbGradeInfo{
+		Id:          uint(data.Id),
+		Title:       data.Title,
+		Description: data.Description,
+		Score:       int(data.Score),
+		Expired:     int(data.Expired),
+		SysCreated:  nil,
+		SysUpdated:  nil,
+	}
+	d.SysCreated = common.TimeParse(data.SysCreated)
+	d.SysUpdated = common.TimeParse(data.SysUpdated)
+	return d
+}
+
+// GradePrivilegeToMessage model转message
+func GradePrivilegeToMessage(data *TbGradePrivilege) *pb.TbGradePrivilege {
+	d := &pb.TbGradePrivilege{
+		Id:          int32(data.Id),
+		GradeId:     int32(data.GradeId),
+		Product:     data.Product,
+		Function:    data.Function,
+		Description: data.Description,
+		Expired:     int32(data.Expired),
+		SysCreated:  "",
+		SysUpdated:  "",
+		SysStatus:   int32(data.SysStatus),
+	}
+	d.SysCreated = common.TimeFormat(data.SysCreated)
+	d.SysUpdated = common.TimeFormat(data.SysUpdated)
+	return d
+}
+
+// GradePrivilegeToObject message转model
+func GradePrivilegeToObject(data *pb.TbGradePrivilege) *TbGradePrivilege {
+	d := &TbGradePrivilege{
+		Id:          uint(data.Id),
+		GradeId:     int(data.GradeId),
+		Product:     data.Product,
+		Function:    data.Function,
+		Description: data.Description,
+		Expired:     int(data.Expired),
+		SysCreated:  nil,
+		SysUpdated:  nil,
+		SysStatus:   int(data.SysStatus),
+	}
+	d.SysCreated = common.TimeParse(data.SysCreated)
+	d.SysUpdated = common.TimeParse(data.SysUpdated)
+	return d
+}
+
+// GradeUserToMessage model转message
+func GradeUserToMessage(data *TbGradeUser) *pb.TbGradeUser {
+	d := &pb.TbGradeUser{
+		Id:         int32(data.Id),
+		Uid:        int32(data.Uid),
+		GradeId:    int32(data.GradeId),
+		Expired:    "",
+		Score:      int32(data.Score),
+		SysCreated: "",
+		SysUpdated: "",
+	}
+	d.Expired = common.TimeFormat(data.Expired)
+	d.SysCreated = common.TimeFormat(data.SysCreated)
+	d.SysUpdated = common.TimeFormat(data.SysUpdated)
+	return d
+}
+
+// GradeUserToObject message转model
+func GradeUserToObject(data *pb.TbGradeUser) *TbGradeUser {
+	d := &TbGradeUser{
+		Id:         uint(data.Id),
+		Uid:        int(data.Uid),
+		GradeId:    int(data.GradeId),
+		Expired:    nil,
+		Score:      int(data.Score),
+		SysCreated: nil,
+		SysUpdated: nil,
+	}
+	d.Expired = common.TimeParse(data.Expired)
+	d.SysCreated = common.TimeParse(data.SysCreated)
+	d.SysUpdated = common.TimeParse(data.SysUpdated)
+	return d
+}
--- ugServer/coin_server.go
+++ ugServer/coin_server.go
@@ -24,7 +24,7 @@ func (u *UgCoinServer) ListTasks(ctx context.Context, in *pb.ListTasksRequest) (
 	coinTaskSvc := service.NewCoinTaskService(ctx)
 	dataList, err := coinTaskSvc.FindAll()
 	if err != nil {
-		return nil, err
+		return nil, status.Errorf(codes.Internal, err.Error())
 	}
 	result := make([]*pb.TbCoinTask, len(dataList))
 	for i, task := range dataList {
@@ -36,18 +36,85 @@ func (u *UgCoinServer) ListTasks(ctx context.Context, in *pb.ListTasksRequest) (
 
 // UserCoinInfo 获取用户的积分信息
 func (u *UgCoinServer) UserCoinInfo(ctx context.Context, in *pb.UserCoinInfoRequest) (*pb.UserCoinInfoReply, error) {
-	//uid := int(in.Uid)
-	//coinTaskSvc := service.New(ctx)
-	//data,err := coinTaskSvc.
-	return nil, status.Errorf(codes.Unimplemented, "method UserCoinInfo not implemented")
+	coinUserSvc := service.NewCoinUserService(ctx)
+	uid := int(in.Uid)
+	data, err := coinUserSvc.GetByUid(uid)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	d := models.CoinUserToMessage(data)
+	out := &pb.UserCoinInfoReply{
+		Data: d,
+	}
+	return out, nil
 }
 
 // UserDetails 获取用户的积分明细列表
 func (u *UgCoinServer) UserDetails(ctx context.Context, in *pb.UserDetailsRequest) (*pb.UserDetailsReply, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UserDetails not implemented")
+	uid := int(in.Uid)
+	page := int(in.Page)
+	size := int(in.Size)
+	coinDetailSvc := service.NewCoinDetailService(ctx)
+	datalist, total, err := coinDetailSvc.FindByUid(uid, page, size)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	dlist := make([]*pb.TbCoinDetail, len(datalist))
+	for i := range datalist {
+		dlist[i] = models.CoinDetailToMessage(&datalist[i])
+	}
+	out := &pb.UserDetailsReply{
+		Datalist: dlist,
+		Total:    int32(total),
+	}
+	return out, nil
 }
 
 // UserCoinChange 调整用户积分-奖励和惩罚都是这个接口
 func (u *UgCoinServer) UserCoinChange(ctx context.Context, in *pb.UserCoinChangeRequest) (*pb.UserCoinChangeReply, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UserCoinChange not implemented")
+	uid := int(in.Uid)
+	task := in.Task
+	coin := int(in.Coin)
+	taskInfo, err := service.NewCoinTaskService(ctx).GetByTask(task)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	//if taskInfo == nil {
+	//	return nil, status.Errorf(codes.Internal, errors.New("r"))
+	//}
+	// 插入详情
+	coinDetail := models.TbCoinDetail{
+		Uid:    uid,
+		TaskId: int(taskInfo.Id),
+		Coin:   coin,
+	}
+	err = service.NewCoinDetailService(ctx).Save(&coinDetail)
+	if err != nil {
+		return nil, err
+	}
+	// 更新用户信息
+	coinUserSvc := service.NewCoinUserService(ctx)
+	coinUser, err := coinUserSvc.GetByUid(uid)
+	if err != nil {
+		return nil, err
+	}
+	if coinUser == nil {
+		coinUser = &models.TbCoinUser{
+			Uid:   uid,
+			Coins: coin,
+		}
+	} else {
+		coinUser.Coins += coin
+		coinUser.SysCreated = nil
+		coinUser.SysUpdated = nil
+	}
+
+	err = coinUserSvc.Save(coinUser)
+	if err != nil {
+		return nil, err
+	}
+	out := &pb.UserCoinChangeReply{
+		User: models.CoinUserToMessage(coinUser),
+	}
+	return out, nil
 }
